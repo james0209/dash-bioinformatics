673638ead9b5a7962dc222e7e0ef8c5b4c558d0f
James Brookes <jpebrookes@gmail.com>
2021-05-22 18:43:15 +0100

    move file locations

--- apps/alignment.py
+++ apps/alignment.py
@@ -1,17 +1,25 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from Bio import SeqIO
 import dash_bio as dashbio
 
 from app import app
 
 # Reads in static FASTA sequence
-alignment_data = open("./SubsetDatabase1.fasta", "r", encoding="utf-8").read()
+file = open("assets/alignments.fasta", "r", encoding="utf-8")
+alignment_data = file.read()
+file.close()
 
 layout = html.Div(
     [
         html.Div(id="container"),
+        html.P(children="This component shows alignments of the sequences present in the alignments FASTA file."),
+        html.Br(),
+        html.P(
+            children="The consensus sequence show at the bottom is the calculated order of most frequent residues found at each position."
+        ),
+        html.Br(),
+        html.P(children="The settings for this chart can be adjusted within the alignment.py file of the application."),
         html.Div(
             children=[
                 html.Div(
@@ -23,12 +31,12 @@ layout = html.Div(
                             extension="fasta",  # If your data contains clustal output, turn it into "clustal".
                             showgap=False,
                             showconsensus=True,  # If you would like to just enable alignment part, turn it into False.
-                            showconservation=False,  # If you would like to just enable alignment part, turn it into False.
+                            showconservation=True,  # If you would like to just enable alignment part, turn it into False.
                             tilewidth=20,  # It defines the width of the each amino acid/nucleotid box.
                             tileheight=10,  # It defines the height of the each amino acid/nucleotid box.
                             showid=False,
                             overview=None,  # If you would like to change, turn it into "heatmap" or "slider".
-                            height=200,
+                            height=400,
                             width="95%",
                         ),
                     ],
